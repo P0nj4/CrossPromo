cd7f2a90363bbe13beee3194437baa8d220eb99b
P0nj4 <german.f.pereyra@gmail.com>
2016-10-03 15:00:17 -0300

    Initial Commit

---
+++ CrossPromo/CrossPromo.h
@@ -0,0 +1,19 @@
+//
+//  CrossPromo.h
+//  CrossPromo
+//
+//  Created by German Pereyra on 10/3/16.
+//  Copyright © 2016 German Pereyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrossPromo.
+FOUNDATION_EXPORT double CrossPromoVersionNumber;
+
+//! Project version string for CrossPromo.
+FOUNDATION_EXPORT const unsigned char CrossPromoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrossPromo/PublicHeader.h>
+
+
